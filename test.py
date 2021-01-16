a706c54ecb0aab04697d65f65ff87383f2b1852b
Paul Dubois <pauldubois98@hotmail.com>
2021-01-16 02:24:34 +0100

    first face merging working version

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
 import cv2
 import numpy as np
 import dlib
+import scipy as sp
+from scipy import ndimage, misc
 
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+def steps(img_in, img_match="profil.jpg"):
+    x1_, y1_, x2_, y2_, pts_match = profil_points(img_match)
+    x1, y1, x2, y2, pts = profil_points(img_in)
+    h_ = abs(y2_-y1_)
+    w_ = abs(x2_-x1_)
+    center_ = np.average(pts_match, axis=0)
+    d_ = np.average(np.abs(pts_match-center_))
+    corners = np.array( ((x1, y1), (x2, y2)) )
+    pts = pts @ R
+    h = abs(y2-y1)
+    w = abs(x2-x1)
+    sw = w_/w
+    sh = h_/h
+    s = (sh+sw)/2
+    center = np.average(pts, axis=0)
+    d = np.average(np.abs(pts-center))
+    s = d_/d
+    scaled_pts = s*pts
+    scaled_corners = s*corners
+    trans = np.average(pts_match-scaled_pts, axis=0)
+    centered_pts = scaled_pts+trans
+    centered_corners = scaled_corners+trans
+    show_points(pts, corners)
+    show_points(scaled_pts, scaled_corners)
+    show_points(centered_pts, centered_corners)
 
 
 def show_points(pts, corners=None, image_name="profil.jpg"):
-    detector = dlib.get_frontal_face_detector()# Load the predictor
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# read the image
+    global detector, predictor 
     img = cv2.imread(image_name)# Convert image into grayscale
     gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)# Use detector to find landmarks
     faces = detector(gray)
@@ -32,9 +63,8 @@ def show_points(pts, corners=None, image_name="profil.jpg"):
     cv2.destroyAllWindows()
 
 def profil_points(image_name="profil.jpg"):
+    global detector, predictor
     pts = np.zeros( (68,2), np.float)
-    detector = dlib.get_frontal_face_detector()# Load the predictor
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")# read the image
     img = cv2.imread(image_name)# Convert image into grayscale
     gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)# Use detector to find landmarks
     faces = detector(gray)
@@ -51,6 +81,37 @@ def profil_points(image_name="profil.jpg"):
         pts[n] = (x,y)
         #cv2.circle(img=img, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
     return (x1, y1, x2, y2, pts)
+
+def points(img):
+    global detector, predictor
+    pts = np.zeros( (68,2), np.float)
+
+    gray = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)# Use detector to find landmarks
+    faces = detector(gray)
+    face = faces[0]
+    x1 = face.left() # left point
+    y1 = face.top() # top point
+    x2 = face.right() # right point
+    y2 = face.bottom() # bottom point
+    # Create landmark object
+    landmarks = predictor(image=gray, box=face)
+    for n in range(0, 68):
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        pts[n] = (x,y)
+        #cv2.circle(img=img, center=(x, y), radius=3, color=(0, 255, 0), thickness=-1)
+    return (x1, y1, x2, y2, pts)
+
+def center(img):
+    x1, y1, x2, y2, pts = points(img)
+    center = np.average(pts, axis=0)
+    return center
+
+def center_points(image_name="profil.jpg"):
+    x1, y1, x2, y2, pts = profil_points(image_name)
+    center = np.average(pts, axis=0)
+    center = pts[0]
+    return center
 
 def rotation(angle):
     theta = np.radians(angle)
@@ -113,39 +174,48 @@ def params(img_in, img_match="profil.jpg"):
     return a, s, v, R, M
 
 
-params("aiden.png")
+def fit_image(image_name, img_match="profil.jpg"):
+    a, s, v, R, M = params(image_name, img_match)
+    img = cv2.imread(image_name)
+    img_ = cv2.imread(img_match)
+    rotated_img = ndimage.rotate(img, a, reshape=False)
+    shape = rotated_img.shape[:2]
+    scaled_shape = (int(s*shape[1]), int(s*shape[0]))
+    scaled_img = cv2.resize(rotated_img, dsize=scaled_shape, interpolation=cv2.INTER_CUBIC)
+    v = center(img_) - center(scaled_img)#recalculate center
+    centered_img = ndimage.shift(scaled_img, shift=(v[1], v[0], 0))
+    shape_match = img_.shape
+    croped_img = np.zeros(shape_match)
+    shape = min(centered_img.shape[0], shape_match[0]), min(centered_img.shape[1], shape_match[1])
+    croped_img[:shape[0],:shape[1], :] = centered_img[:shape[0],:shape[1], :]/255
+
+    # cv2.imshow(winname="Face", mat=img)
+    # cv2.waitKey(delay=0)
+    # cv2.imshow(winname="Face", mat=rotated_img)
+    # cv2.waitKey(delay=0)
+    # cv2.imshow(winname="Face", mat=scaled_img)
+    # cv2.waitKey(delay=0)
+    # cv2.imshow(winname="Face", mat=centered_img)
+    # cv2.waitKey(delay=0)
+    # cv2.destroyAllWindows()
+    
+    return croped_img
 
 
+def merge_images(image_names):
+    fitted_imgs = [fit_image(image_name) for image_name in image_names]
+    merged_img = np.average( fitted_imgs, axis=0)
+    # for fitted_img in fitted_imgs:
+    #     cv2.imshow(winname="Face", mat=fitted_img)
+    #     cv2.waitKey(delay=0)
+    # cv2.imshow(winname="Face", mat=merged_img)
+    # cv2.waitKey(delay=0)
+    # cv2.destroyAllWindows()
+    return merged_img
 
 
-
-
-
-def steps(img_in, img_match="profil.jpg"):
-    x1_, y1_, x2_, y2_, pts_match = profil_points(img_match)
-    x1, y1, x2, y2, pts = profil_points(img_in)
-    h_ = abs(y2_-y1_)
-    w_ = abs(x2_-x1_)
-    center_ = np.average(pts_match, axis=0)
-    d_ = np.average(np.abs(pts_match-center_))
-    corners = np.array( ((x1, y1), (x2, y2)) )
-    pts = pts @ R
-    h = abs(y2-y1)
-    w = abs(x2-x1)
-    sw = w_/w
-    sh = h_/h
-    s = (sh+sw)/2
-    center = np.average(pts, axis=0)
-    d = np.average(np.abs(pts-center))
-    s = d_/d
-    scaled_pts = s*pts
-    scaled_corners = s*corners
-    trans = np.average(pts_match-scaled_pts, axis=0)
-    centered_pts = scaled_pts+trans
-    centered_corners = scaled_corners+trans
-    show_points(pts, corners)
-    show_points(scaled_pts, scaled_corners)
-    show_points(centered_pts, centered_corners)
-
-
+if __name__=="__main__":
+    #merge_images( ("aiden.png", "profil.jpg") )
+    merged_img = merge_images( ("aiden.png", "paul.jpg") )
+    cv2.imwrite('aiden_paul.jpg', merged_img*255)
 
